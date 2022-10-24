30802f7d8801fd987158e0030572408e1e71fbd2
DhuxxMoto22 <115643381+DhuxxMoto22@users.noreply.github.com>
2022-10-24 23:39:05 +0800

    Settings All Updated Latest

---
+++ BloodCare Latest/BloodCare2/app/src/main/java/com/example/bloodcare/PersonalaccFragment.java
@@ -0,0 +1,39 @@
+package com.example.bloodcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class PersonalaccFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_personalacc, container, false);
+
+        //go back to privacy settings
+
+        Button back = (Button) view.findViewById(R.id.back2);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Privacy.class);
+                in.putExtra("some", "some data");
+                startActivity(in);
+
+            }
+        });
+
+        return view;
+
+    }
+}
---
+++ BloodCare Latest/BloodCare2/app/src/main/java/com/example/bloodcare/passwordsecfragment.java
@@ -0,0 +1,40 @@
+package com.example.bloodcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class passwordsecfragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_passwordsecfragment, container, false);
+
+        //go back to privacy settings
+
+        Button back5 = (Button) view.findViewById(R.id.back5);
+        back5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ins = new Intent(getActivity(), Privacy.class);
+                ins.putExtra("some", "some data");
+                startActivity(ins);
+
+            }
+        });
+
+        return view;
+
+
+    }
+
+}
